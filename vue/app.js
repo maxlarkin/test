4a517ca69edbec1523780d8ef87c7f9c1c36135d
Evgeniy Medvedev <jack123456789@mail.ru>
2019-11-20 10:35:52 +0400

    first commit

---
+++ vue/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#blog-post-demo',
+    data: {
+        post: [
+            { id: 1, title: 'My journey with Vue' },
+            { id: 2, title: 'Blogging with Vue' },
+            { id: 3, title: 'Why Vue is so fun' }
+        ]
+    }
+})
