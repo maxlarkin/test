ad0386c09ec02966c5e1586789ae4cf272639b14
Evgeniy Medvedev <jack123456789@mail.ru>
2019-11-24 18:47:48 +0400

    user commit

--- PROJECT/vue/functions.js
+++ PROJECT/vue/functions.js
@@ -1,9 +1,44 @@
+Vue.component('tab-пользователь', {
+    data: function () {
+        return {
+            posts: [
+                {user: 'id - 425783999'},
+                {user: 'имя - Андрей'},
+                {user: 'фамилия - Лермонтов'},
+                {user: 'отчество - Сергеевич'},
+                {user: 'должность - сварщик'}
+            ],
+            selectedPost: null
+        }
+    },
+    template: `
+  	<div class="posts-tab">
+      <div class="posts-sidebar">
+        <p
+          v-for="post in posts"
+          v-bind:class="{ selected: post === selectedPost }"
+					v-on:click="selectedПользователь = пользователь"
+        >
+          {{ post.user }}
+        </p>
+      </div>
+    </div>
+  `
+})
+
+Vue.component('tab-данные', {
+    template: '<div><h2>e-mail - maxlarkin2007@gmail.com<br>телефон - 8 982 122 33 50<br>адрес - александровская 19<br>пол - мужской<br>возраст - 23</h2></div>'
+})
+
 new Vue({
-    el: '#knop',
+    el: '#dynamic-component-demo',
     data: {
-        knops: [
-            {user: 'пользователь' },
-            {date: 'данные'}
-        ]
+        currentTab: 'Posts',
+        tabs: ['пользователь', 'данные']
+    },
+    computed: {
+        currentTabComponent: function () {
+            return 'tab-' + this.currentTab.toLowerCase()
+        }
     }
 })
