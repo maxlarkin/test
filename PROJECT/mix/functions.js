d1ffc825b38e8af4471db579b4bc38465fb91268
Evgeniy Medvedev <jack123456789@mail.ru>
2019-11-26 20:53:36 +0400

    test commit

--- PROJECT/mix/functions.js
+++ PROJECT/mix/functions.js
@@ -1,25 +1,73 @@
-new Vue({
-    el: '.container',
+var app = new Vue ({
+    el: '#app',
+    data: {
+        message: 'привет Vue'
+    }
+})
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'дата: ' + new Date().toLocaleString()
+    }
+})
+var app3 = new Vue({
+    el: '#app-3',
+    data:{
+        seen: true
+    }
+})
+var app4 = new Vue({
+    el: '#app-4',
     data: {
         users: [
-            {name: 'пользователь'}
-        ],
-        dates: [
-            {name: 'данные'}
+            { text: 'Изучить JavaScript' },
+            { text: 'Изучить Vue' },
+            { text: 'Создать что-нибудь классное' }
         ]
     }
 })
-$(document).ready(function () {
-
-    $(document).on('click', '.tabs a', function (e) {
-        e.preventDefault();
-        let link = $(this);
-
-        let href = link.attr('href');
-        // alert(href);
-        // console.log("link: ", link);
-        $('.tab-content > *, .tabs ul li').removeClass('active');
-        $(href).addClass('active');
-        link.parent().addClass('active')
-    })
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Vue'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
 })
+
+
+
+
+
+
+
+
+
+// new Vue({
+//     el: '.container',
+//     data: {
+//         users: [
+//             {name: 'пользователь'}
+//         ],
+//         dates: [
+//             {name: 'данные'}
+//         ]
+//     }
+// })
+// $(document).ready(function () {
+//
+//     $(document).on('click', '.tabs a', function (e) {
+//         e.preventDefault();
+//         let link = $(this);
+//
+//         let href = link.attr('href');
+//         // alert(href);
+//         // console.log("link: ", link);
+//         $('.tab-content > *, .tabs ul li').removeClass('active');
+//         $(href).addClass('active');
+//         link.parent().addClass('active')
+//     })
+// })
